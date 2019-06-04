Fix a bug with all 0 labels by adding a flag to control how to handle it. Revise the computation of validation and test metrics Add "objective_metric" to select model according to a specific metric

input_layer/DirectLabelFeed.py
@@ -52,19 +52,22 @@ class DirectLabelFeed:
         self.batch_size = batch_size
         self.model = model
         
-    def prepare_true_labels_with_index(self, data_set, index, docid_inputs, letor_features, labels):
+    def prepare_true_labels_with_index(self, data_set, index, docid_inputs, letor_features, labels, check_validation=True):
         i = index
+        # Generate label list.
+        label_list = [0 if data_set.initial_list[i][x] < 0 else data_set.labels[i][x] for x in range(len(data_set.initial_list[i]))]
+
+        # Check if data is valid
+        if check_validation and sum(label_list) == 0:
+            return
         base = len(letor_features)
         for x in data_set.initial_list[i]:
             if x >= 0:
                 letor_features.append(data_set.features[x])
         docid_inputs.append(list([-1 if data_set.initial_list[i][x] < 0 else base+x for x in range(len(data_set.initial_list[i]))]))
-            
-        # Generate clicks with click models.
-        label_list = [0 if data_set.initial_list[i][x] < 0 else data_set.labels[i][x] for x in range(len(data_set.initial_list[i]))]
         labels.append(label_list)
     
-    def get_batch(self, data_set):
+    def get_batch(self, data_set, check_validation=True):
         """Get a random batch of data, prepare for step. Typically used for training.
 
         To feed data in step(..) it must be a list of batch-major vectors, while
@@ -73,6 +76,7 @@ class DirectLabelFeed:
 
         Args:
             data_set: (Raw_data) The dataset used to build the input layer.
+            check_validation: (bool) Set True to ignore data with no positive labels.
 
         Returns:
             input_feed: a feed dictionary for the next step
@@ -90,10 +94,11 @@ class DirectLabelFeed:
             i = int(random.random() * length)
             rank_list_idxs.append(i)
             self.prepare_true_labels_with_index(data_set, i,
-                                docid_inputs, letor_features, labels)
+                                docid_inputs, letor_features, labels, check_validation)
 
+        local_batch_size = len(docid_inputs)
         letor_features_length = len(letor_features)
-        for i in range(self.batch_size):
+        for i in range(local_batch_size):
             for j in range(self.rank_list_size):
                 if docid_inputs[i][j] < 0:
                     docid_inputs[i][j] = letor_features_length
@@ -105,11 +110,11 @@ class DirectLabelFeed:
             # Batch encoder inputs are just re-indexed docid_inputs.
             batch_docid_inputs.append(
                 np.array([docid_inputs[batch_idx][length_idx]
-                    for batch_idx in range(self.batch_size)], dtype=np.float32))
+                    for batch_idx in range(local_batch_size)], dtype=np.float32))
             # Batch decoder inputs are re-indexed decoder_inputs, we create labels.
             batch_labels.append(
                 np.array([labels[batch_idx][length_idx]
-                        for batch_idx in range(self.batch_size)], dtype=np.float32))
+                        for batch_idx in range(local_batch_size)], dtype=np.float32))
         # Create input feed map
         input_feed = {}
         input_feed[self.model.letor_features.name] = np.array(letor_features)
@@ -126,7 +131,7 @@ class DirectLabelFeed:
 
         return input_feed, info_map
 
-    def get_next_batch(self, index, data_set):
+    def get_next_batch(self, index, data_set, check_validation=True):
         """Get the next batch of data from a specific index, prepare for step. 
            Typically used for validation.
 
@@ -137,6 +142,7 @@ class DirectLabelFeed:
         Args:
             index: the index of the data before which we will use to create the data batch.
             data_set: (Raw_data) The dataset used to build the input layer.
+            check_validation: (bool) Set True to ignore data with no positive labels.
 
         Returns:
             input_feed: a feed dictionary for the next step
@@ -150,11 +156,11 @@ class DirectLabelFeed:
         docid_inputs, letor_features, labels = [], [], []
         
         num_remain_data = len(data_set.initial_list) - index
-        local_batch_size = self.batch_size if num_remain_data > self.batch_size else num_remain_data
-        for offset in range(local_batch_size):
+        for offset in range(min(self.batch_size, num_remain_data)):
             i = index + offset
-            self.prepare_true_labels_with_index(data_set, i, docid_inputs, letor_features, labels)
+            self.prepare_true_labels_with_index(data_set, i, docid_inputs, letor_features, labels, check_validation)
 
+        local_batch_size = len(docid_inputs)
         letor_features_length = len(letor_features)
         for i in range(local_batch_size):
             for j in range(self.rank_list_size):
@@ -187,15 +193,13 @@ class DirectLabelFeed:
 
         return input_feed, others_map
 
-    def get_data_by_index(self, data_set, index): 
+    def get_data_by_index(self, data_set, index, check_validation=False): 
         """Get one data from the specified index, prepare for step.
 
                 Args:
-                    input_seq: a list of initial ranking ([0,1,2,3,4...])
-                    output_seq: the target ranking list ([2,3,1,0,4,...])
-                    output_weights: the weight list of each output
-                    features: a list of feature vectors for initial ranking list
+                    data_set: (Raw_data) The dataset used to build the input layer.
                     index: the index of the data
+                    check_validation: (bool) Set True to ignore data with no positive labels.
 
                 Returns:
                     The triple (docid_inputs, decoder_inputs, target_weights) for
@@ -208,13 +212,12 @@ class DirectLabelFeed:
         docid_inputs, letor_features, labels = [], [], []
         
         i = index
-        self.prepare_true_labels_with_index(data_set, i, docid_inputs, letor_features, labels)
+        self.prepare_true_labels_with_index(data_set, i, docid_inputs, letor_features, labels, check_validation)
 
         letor_features_length = len(letor_features)
-        for i in range(self.batch_size):
-            for j in range(self.rank_list_size):
-                if docid_inputs[i][j] < 0:
-                    docid_inputs[i][j] = letor_features_length
+        for j in range(self.rank_list_size):
+            if docid_inputs[-1][j] < 0:
+                docid_inputs[-1][j] = letor_features_length
 
         batch_docid_inputs = []
         batch_labels = []
@@ -222,11 +225,11 @@ class DirectLabelFeed:
             # Batch encoder inputs are just re-indexed docid_inputs.
             batch_docid_inputs.append(
                 np.array([docid_inputs[batch_idx][length_idx]
-                    for batch_idx in range(self.batch_size)], dtype=np.float32))
+                    for batch_idx in range(1)], dtype=np.float32))
             # Batch decoder inputs are re-indexed decoder_inputs, we create weights.
             batch_labels.append(
                 np.array([labels[batch_idx][length_idx]
-                        for batch_idx in range(self.batch_size)], dtype=np.float32))
+                        for batch_idx in range(1)], dtype=np.float32))
         # Create input feed map
         input_feed = {}
         input_feed[self.model.letor_features.name] = np.array(letor_features)

learning_algorithm/ipw_rank.py
@@ -133,12 +133,12 @@ class IPWrank(BasicAlgorithm):
                 for topn in self.exp_settings['metrics_topn']:
                     list_weights = tf.reduce_mean(reshaped_propensity * clipped_labels, axis=1, keep_dims=True)
                     metric_value = metrics.make_ranking_metric_fn(metric, topn)(reshaped_labels, self.output, list_weights)
-                    tf.summary.scalar('Weighted_%s@%d' % (metric.upper(), topn), metric_value, collections=['train'])
+                    tf.summary.scalar('Weighted_%s_%d' % (metric, topn), metric_value, collections=['train'])
         
         for metric in self.exp_settings['metrics']:
             for topn in self.exp_settings['metrics_topn']:
                 metric_value = metrics.make_ranking_metric_fn(metric, topn)(reshaped_labels, self.output, None)
-                tf.summary.scalar('%s@%d' % (metric.upper(), topn), metric_value, collections=['train', 'eval'])
+                tf.summary.scalar('%s_%d' % (metric, topn), metric_value, collections=['train', 'eval'])
 
         tf.summary.scalar('Loss', tf.reduce_mean(self.loss), collections=['train'])
 

main.py
@@ -31,7 +31,7 @@ tf.app.flags.DEFINE_string("output_dir", "./tmp_output/", "The directory to outp
 
 # model 
 #tf.app.flags.DEFINE_string("click_model_json", "", "Josn file for the click model used to generate clicks.")
-tf.app.flags.DEFINE_string("setting_file", "./example/ipw_rank_exp_settings.json", "A json file that contains all the settings of the algorithm.")
+tf.app.flags.DEFINE_string("setting_file", "./example/dla_exp_settings.json", "A json file that contains all the settings of the algorithm.")
 #tf.app.flags.DEFINE_boolean("use_non_clicked_data", False,
 #                            "Set to True for estimating propensity weights for non-click data.")
 
@@ -109,7 +109,7 @@ def train(exp_settings):
         step_time, loss = 0.0, 0.0
         current_step = 0
         previous_losses = []
-        best_loss = None
+        best_perf = None
         while True:
             # Get a batch and make a step.
             start_time = time.time()
@@ -127,26 +127,40 @@ def train(exp_settings):
                              "%.2f" % (model.global_step.eval(), model.learning_rate.eval(),
                                                  step_time, loss))
                 previous_losses.append(loss)
-
                 # Validate model
                 it = 0
                 count_batch = 0.0
                 valid_loss = 0
+                summary_list = []
+                batch_size_list = []
                 while it < len(valid_set.initial_list):
-                    input_feed, _ = valid_input_feed.get_next_batch(it, valid_set)
+                    input_feed, info_map = valid_input_feed.get_next_batch(it, valid_set)
                     v_loss, _, summary = model.step(sess, input_feed, True)
+                    summary_list.append(summary)
+                    batch_size_list.append(len(info_map['input_list']))
                     it += valid_input_feed.batch_size
                     valid_loss += v_loss
                     count_batch += 1.0
-                valid_writer.add_summary(summary, current_step)
+                valid_summary = utils.merge_TFSummary(summary_list, batch_size_list)
+                valid_writer.add_summary(valid_summary, current_step)
                 valid_loss /= count_batch
-                print("  eval: loss %.2f" % (valid_loss))
+                print("  eval: %s" % (
+                    ' '.join(['%s:%.3f' % (x.tag, x.simple_value) for x in valid_summary.value])
+                ))
 
-                # Save checkpoint and zero timer and loss. # need to rethink
-                #if best_loss == None or best_loss >= eval_ppx:
-                #    best_loss = eval_ppx
-                checkpoint_path = os.path.join(FLAGS.model_dir, "DLA.ckpt")
-                model.saver.save(sess, checkpoint_path, global_step=model.global_step)
+                # Save checkpoint if the objective metric on the validation set is better
+                if "objective_metric" in exp_settings:
+                    for x in valid_summary.value:
+                        if x.tag == exp_settings["objective_metric"]:
+                            if best_perf == None or best_perf < x.simple_value:
+                                checkpoint_path = os.path.join(FLAGS.model_dir, "DLA.ckpt")
+                                model.saver.save(sess, checkpoint_path, global_step=model.global_step)
+                                best_perf = x.simple_value
+                            break
+                # Save checkpoint if there is no objective metic
+                if best_perf == None:
+                    checkpoint_path = os.path.join(FLAGS.model_dir, "DLA.ckpt")
+                    model.saver.save(sess, checkpoint_path, global_step=model.global_step)
                 if loss == float('inf'):
                     break
 
@@ -174,18 +188,26 @@ def test(exp_settings):
         #test_input_feed = input_layer.DirectLabelFeed(model, 1, exp_settings['test_input_hparams'])
         test_input_feed = utils.find_class(exp_settings['test_input_feed'])(model, 1, exp_settings['test_input_hparams'])
 
+        test_writer = tf.summary.FileWriter(FLAGS.model_dir + '/test_log')
+
         rerank_scores = []
 
         # Decode from test data.
+        summary_list = []
         for i in range(len(test_set.initial_list)):
             input_feed, _ = test_input_feed.get_data_by_index(test_set, i)
             _, output_logits, summary = model.step(sess, input_feed, True)
-
+            summary_list.append(summary)
             #The output is a list of rerank index for decoder_inputs (which represents the gold rank list)
             rerank_scores.append(output_logits[0])
             if i % FLAGS.steps_per_checkpoint == 0:
                 print("Testing %.2f \r" % (float(i)/len(test_set.initial_list))),
-
+        # TODO merge summary
+        test_summary = utils.merge_TFSummary(summary_list, np.ones(len(test_set.initial_list)))
+        test_writer.add_summary(test_summary, i)
+        print("  eval: %s" % (
+                ' '.join(['%s:%.3f' % (x.tag, x.simple_value) for x in test_summary.value])
+        ))
         #get rerank indexes with new scores
         rerank_lists = []
         for i in range(len(rerank_scores)):