ba5dab4cc371692e1950f4a514c21d58de3a71e1
Qingyao Ai <aiqy@cs.umass.edu>
2020-04-01 16:29:09 -0600

    Update create_documentation.sh

--- docsource/create_documentation.sh
+++ docsource/create_documentation.sh
@@ -4,7 +4,10 @@ python ultra/input_layer/parameter_readme_generator.py
 python ultra/learning_algorithm/parameter_readme_generator.py
 python ultra/ranking_model/parameter_readme_generator.py
 
+# Enter doc source folder.
 cd docsource
+# Use sphinx autodoc to generate rst.
+# usage: sphinx-apidoc [OPTIONS] -o <OUTPUT_PATH> <MODULE_PATH> [EXCLUDE_PATTERN,...]
 sphinx-apidoc -o source/ ../ultra
 
 # make html files
