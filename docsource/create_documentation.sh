3d01d1a0c6437bf6b7ca42243f40d589f3c7804b
Qingyao Ai <aiqy@cs.umass.edu>
2020-04-01 16:28:05 -0600

    Update create_documentation.sh

--- docsource/create_documentation.sh
+++ docsource/create_documentation.sh
@@ -4,9 +4,9 @@ python ultra/input_layer/parameter_readme_generator.py
 python ultra/learning_algorithm/parameter_readme_generator.py
 python ultra/ranking_model/parameter_readme_generator.py
 
-cd docs
+cd docsource
 sphinx-apidoc -o source/ ../ultra
 
 # make html files
 make clean
-make html
+make github
