394d494b4ee9873d028c1c27d04956c9301540b1
Tao Yang <52364987+Taosheng-ty@users.noreply.github.com>
2020-04-24 22:36:33 -0600

    update __init__ to incoporate DLCM and Setrank

--- ultra/ranking_model/__init__.py
+++ ultra/ranking_model/__init__.py
@@ -3,7 +3,8 @@ from __future__ import absolute_import
 from .base_ranking_model import *
 from .dnn import *
 from .linear import *
-
+from .DLCM import *
+from .Setrank import *
 def list_available() -> list:
     from .base_ranking_model import BaseRankingModel
     from ultra.utils.sys_tools import list_recursive_concrete_subclasses
