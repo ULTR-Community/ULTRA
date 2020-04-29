2aba964d3c312d1baaf2562d6028e1e2d01de47b
Taosheng-ty <taoyang@cs.utah.edu>
2020-04-29 11:37:55 -0600

    minor debug

--- ultra/ranking_model/__init__.py
+++ ultra/ranking_model/__init__.py
@@ -1,11 +1,13 @@
 # note:
 from __future__ import absolute_import
 from .base_ranking_model import *
-from .dnn import *
-from .linear import *
+
+from .DNN import *
+from .Linear import *
 from .DLCM import *
 from .GSF import *
 from .SetRank import *
+
 def list_available() -> list:
     from .base_ranking_model import BaseRankingModel
     from ultra.utils.sys_tools import list_recursive_concrete_subclasses
